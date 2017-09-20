a5005b24f0e37a136d7b34749329a3d1e5a70ea8
aluno <aluno@estacio.br>
2017-09-19 23:00:17 -0300

    Layout

--- src/TarefaLayout/FrameBorderLayout.java
+++ src/TarefaLayout/FrameBorderLayout.java
@@ -22,16 +22,16 @@ public class FrameBorderLayout extends JFrame {
         JPanel L = new JPanel();
         
 	
-	JButton B1 = new JButton ("Botão 1");
-        JButton B2 = new JButton ("Botão 2");
-        JButton B3 = new JButton ("Botão 3");
-        JButton B4 = new JButton ("Botão 4");
-        JButton B5 = new JButton ("Botão 5");
-        JButton B6 = new JButton ("Botão 6");
-        JButton B7 = new JButton ("Botão 7");
-        JButton B8 = new JButton ("Botão 8");
-        JButton B9 = new JButton ("Botão 9");
-        JButton B0 = new JButton ("Botão 0");
+	JButton BT1 = new JButton ("Botão 1");
+        JButton BT2 = new JButton ("Botão 2");
+        JButton BT3 = new JButton ("Botão 3");
+        JButton BT4 = new JButton ("Botão 4");
+        JButton BT5 = new JButton ("Botão 5");
+        JButton BT6 = new JButton ("Botão 6");
+        JButton BT7 = new JButton ("Botão 7");
+        JButton BT8 = new JButton ("Botão 8");
+        JButton BT9 = new JButton ("Botão 9");
+        JButton BT0 = new JButton ("Botão 0");
         
 	//Localização de cada Painel
 	JLabel L1 = new JLabel(" Parte Sul");
@@ -39,13 +39,57 @@ public class FrameBorderLayout extends JFrame {
         JLabel L3 = new JLabel(" Parte Oeste");
         JLabel L4 = new JLabel(" Parte Leste");
         JLabel L5 = new JLabel(" Parte Norte");
-        
+                
 	//Layouts
-	BorderLayout m = new BorderLayout();
-        BorderLayout g = new BorderLayout();
-        BorderLayout f = new BorderLayout();
-        BorderLayout c = new BorderLayout();
-        BorderLayout t = new BorderLayout();
+	BorderLayout B = new BorderLayout();
+        BorderLayout S = new BorderLayout();
+        BorderLayout Y = new BorderLayout();
+        BorderLayout O = new BorderLayout();
+        BorderLayout CENTRO = new BorderLayout();
         
+	//Propriedades
+        super.setTitle("Exercicio Programação ");
+        
+        P.setLayout(B);
+        P.add(BT1, B.NORTH);
+        P.add(BT2, B.SOUTH);
+        P.add(L5, B.CENTER);
+        add(P, B.NORTH);
+        
+        A.setLayout(S);
+        A.add(BT3, S.NORTH);
+        A.add(BT4, S.SOUTH);
+        A.add(L1, S.CENTER);
+        add(A, S.SOUTH);
+        
+        N.setLayout(Y);
+        N.add(BT5, Y.NORTH);
+        N.add(BT6, Y.SOUTH);
+        N.add(L4, Y.CENTER);
+        add(N, Y.EAST);
+        
+        E.setLayout(O);
+        E.add(BT7, O.NORTH);
+        E.add(BT8, O.SOUTH);
+        E.add(L3, O.CENTER);
+        add(E, O.WEST);
+        
+        L.setLayout(CENTRO);
+        L.add(BT9, CENTRO.NORTH);
+        L.add(BT0, CENTRO.SOUTH);
+        L.add(L2, CENTRO.CENTER);
+        add(L, CENTRO.CENTER);
+        
+        
+        
+   }
+	//Execução do Aplicativo java
+      public static void main(String args[]) {
+        FrameBorderLayout B = new FrameBorderLayout();
+        B.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        B.setSize(500,250); 
+        B.setVisible(true);
     }
+
 }
+
---
+++ src/eventos/TestaEventos.java
@@ -0,0 +1,9 @@
+
+package eventos;
+
+
+public class TestaEventos  extends JFrame implements ActionListener {
+    
+    @Override
+    public void actionPerformed (ActionEvent)
+}
